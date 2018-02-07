0c223d6cc5ef3c66afcbc85ba92cb291cbfc046a
Niko Oshinov <niko.oshinov@gmail.com>
2018-02-06 20:04:50 -0500

    Added unit test module

--- data_gen.py
+++ data_gen.py
@@ -1,13 +1,18 @@
 import requests
 from lxml.html import fromstring
 
-URL = ("https://api.findmespot.com/spot-main-web/consumer/" + 
-	"rest-api/2.0/public/feed/0i9SOoPmAWhrgM15Yl41McqjrEbERSGiD/message.xml")
 
 class ScrapeData():
 	"""Downloads data from API and manipulates it into a list of locations"""
-	def main():
-		raw_data = requests.get(URL).content # Downloads data from API
+	URL = ("https://api.findmespot.com/spot-main-web/consumer/" + 
+		"rest-api/2.0/public/feed/0i9SOoPmAWhrgM15Yl41McqjrEbERSGiD/message.xml")
+
+	def main(self):
 		root = fromstring(raw_data)
 		messages = root[0].xpath('//message')
 
+	def get_xml(self):
+	"""
+	Makes an API Call and returns data as string
+	"""
+		return requests.get(URL).content
