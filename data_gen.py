30c84f7f5bbb5a8782170e638d98fe1177377228
Niko Oshinov <niko.oshinov@gmail.com>
2018-02-08 14:57:25 -0500

    Added point specific names, descriptions, and icons

--- data_gen.py
+++ data_gen.py
@@ -13,9 +13,6 @@ class ScrapeData():
 		'custm_msg': 8,'dtime': 9, 'bat': 10,'hidden': 11,
 		'alt': 12
 		}
-		# Not necessary: included because it was in the old file
-		self.description_str = "<![CDATA[Date: ]]><br/><![CDATA[Time: UTC]]>"
-
 
 	def get_xml(self, URL):
 		"""
@@ -29,39 +26,96 @@ class ScrapeData():
 		"""
 		return fromstring(XML).xpath('//message')
 
-	def create_point(self, kml, msg_data, style=Style()):
+	def create_point(self, kml, msg_data, position_num=None, style=Style()):
 		"""
 		returns a kml.newpoint object of the form
 		"""
 		# name, altitude mode have to be set in initialization 
-		point = kml.newpoint(name=self.get_time(msg_data), 
-			altitudemode='clampedToGround') 
+		point = kml.newpoint(name=self.get_time(msg_data, 
+			position_num=position_num), altitudemode='clampedToGround') 
 		point.coords = self.get_coords(msg_data)
 		point.style = style
-		point.description = self.description_str
+		point.description = self.get_description(msg_data)
 		return point
 
 	def get_coords(self,msg_data):
 		return [(msg_data[self.indx['lat']].text, msg_data[self.indx['long']].text)]
 
-	def get_time(self, msg_data):
-		""" currently missing 'position #' format """
+	def get_time(self, msg_data, position_num = None):
+		"""Sets the time and of a point and enumerates it via position #"""
 		# -13 and -8 indexes are consistent across all formats 
-		return msg_data[self.indx['dtime']].text[-13:-8]
+		prefix = ''
+		if position_num != None:
+			prefix = "Position %s at " % position_num
+		return prefix + msg_data[self.indx['dtime']].text[-13:-8]
+
+	def create_lines_and_base_obj(self, msg_list):
+		"""
+		Returns KML object with a chain of lines representing the order
+		in which msg_list items are indexed.
+		"""
+		kml = Kml()
+		for i in range(1, len(msg_list)):
+			self.create_line_sgmnt(kml, msg_list[i-1], msg_list[i])	
+		return kml
+
+	def get_line_link_coords(self, msg_data1, msg_data2):
+		"""
+		Extracts coordinates for creating a LineString and returns
+		a list of two coordinate tuples.  i.e. 'links' a pair of 
+		messages' coordinates so they can be made into a LineString
+		"""
+		return [(
+			msg_data1[self.indx['lat']].text, 
+			msg_data1[self.indx['long']].text), (
+			msg_data2[self.indx['lat']].text, 
+			msg_data2[self.indx['long']].text)]
+
+	def create_line_sgmnt(self, kml, msg_list1, msg_list2):
+		"""
+		Given full data represnting two distinct messages, returns a line 
+		segment drawn between the two.
+		"""
+		line = kml.newlinestring(name='line segment', 
+			altitudemode='clampedToGround')
+		line.coords = self.get_line_link_coords(msg_list1, msg_list2)
+		line.extrude = 1 # connects point to ground
+		return line
 
 	def give_kml_obj(self, msg_list):
-		kml = Kml()
+		"""
+		Initializes Kml file by calling another class function, creates
+		balloon style, and plots points. Line segments are created on
+		initialization. Returns a Kml object ready to be saved.
+		"""
+		kml = self.create_lines_and_base_obj(msg_list)
 		style = Style()
-		style.iconstyle.icon.href = (
-			'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png')
+		position_num = 50
 		for msg_data in msg_list:
-			self.create_point(kml, msg_data, style)
+			style = self.get_point_style(style, position_num)
+			self.create_point(kml, msg_data, position_num, style=style)
+			position_num = position_num - 1 
 		return kml
 
 	def save_kml_obj(self, URL, DIR):
 		msg_list = self.get_list_of_msgs(self.get_xml(URL))
 		self.give_kml_obj(msg_list).save(DIR)
 
+	def get_description(self,msg_data):
+		"""Description string on point focus"""
+		description_str= ("<![CDATA[Date: %s]]><br/><![CDATA[Time: UTC]]>" 
+			%msg_data[self.indx['dtime']].text[:10])
+		return description_str
+	def get_point_style(self, style, position_num):
+		"""sets the last icon as a plane, all others as simple dots"""
+		style.iconstyle.icon.href = ('http://maps.google.com/' + 
+			'mapfiles/kml/shapes/placemark_circle.png')
+		if position_num == 1:
+			style = Style()
+			style.iconstyle.icon.href = (
+				'http://maps.google.com/mapfiles/kml/pal2/icon48.png')
+		return style
+		
 DIR = join(dirname(realpath(__file__)), 'coordinates.kml')
 URL = ("https://api.findmespot.com/spot-main-web/consumer/" + 
 		"rest-api/2.0/public/feed/0i9SOoPmAWhrgM15Yl41McqjrEbERSGiD/" + 
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 from lxml.etree import fromstring
 from data_gen import ScrapeData
-from simplekml import Kml, Coordinates
+from simplekml import Kml, Coordinates, LineString
 
 class TestScraper(unittest.TestCase):
 
@@ -59,7 +59,8 @@ class TestScraper(unittest.TestCase):
 			'UNLIMITED-TRACK', '-8.74288', '-74.43631', 'SPOT3', 'Y', 
 			'2018-02-06T18:13:27+0000', 'GOOD', '0', '2027'])
 		self.assertEqual(self.scraper.get_time(self.sample_row1), '18:23')
-		self.assertEqual(self.scraper.get_time(sample_row2), '18:13')
+		self.assertEqual(self.scraper.get_time(sample_row2, position_num=3), 
+			'Position 3 at 18:13')
 
 	def test_correct_pt_time(self):
 		kml = Kml()
@@ -67,7 +68,6 @@ class TestScraper(unittest.TestCase):
 		self.assertEqual(point.name, '18:23')
 
 	def test_sets_altitudemode(self):
-		# Not sure if working as intended -- Check later ------------------------------------
 		kml = Kml()
 		point = self.scraper.create_point(kml, self.sample_row1)
 		self.assertEqual(point.altitudemode, 'clampedToGround')
@@ -81,11 +81,35 @@ class TestScraper(unittest.TestCase):
 		point = self.scraper.create_point(kml, self.sample_row1)
 		self.assertIsInstance(point.coords, Coordinates)
 	
+	def test_set_description(self):
+		date = self.sample_row1[self.indx['dtime']].text[:10]
+		description_str= ("<![CDATA[Date: %s]]><br/><![CDATA[Time: UTC]]>" % date)
+		self.assertEqual(self.scraper.get_description(self.sample_row1),
+			description_str)
+
+	def test_line_sgmnt(self):
+		""" 
+		The segment specs are private variables. This tests whetehr line
+		segments can be made.
+		"""
+		sample_row2 = self.convert_list_to_etree(
+			['913686008', '0-2440482', 'Lima', '1517940807', 
+			'UNLIMITED-TRACK', '-8.74288', '-74.43631', 'SPOT3', 'Y', 
+			'2018-02-06T18:13:27+0000', 'GOOD', '0', '2027'])
+		kml = Kml()
+		line = self.scraper.create_line_sgmnt(kml, self.sample_row1,
+			sample_row2)
+		self.assertIsInstance(line, LineString)
+
 	def test_is_ascii(self):
+		"""
+		Tests are done without http requests; static files are used instead.
+		This test makes sure that static files are valid (simplekml doesn't
+		like higher order characters).
+		"""
 		with open('example_xml.xml','r') as f:
 			XML = fromstring(f.read())
 			self.assertTrue(all(ord(c) < 128 for c in str(XML)))
-
 	def convert_list_to_etree(self, list_):
 		"""
 		used to conert test strings to the type of 
