6f35b38783ab5c0d449f7bb24313a6cf5bd717ea
Niko Oshinov <niko.oshinov@gmail.com>
2018-02-07 22:04:42 -0500

    Added test & function for getting time. Intentional 'clutter' code which will be deleted is present

--- data_gen.py
+++ data_gen.py
@@ -7,6 +7,13 @@ class ScrapeData():
 	def __init__(self):
 		URL = ("https://api.findmespot.com/spot-main-web/consumer/" + 
 			"rest-api/2.0/public/feed/0i9SOoPmAWhrgM15Yl41McqjrEbERSGiD/message.xml")
+		# consider refactoring so indexes are passed as arguments
+		self.indx = {
+		'id': 0, 'msngr_id': 1,'msngr_name': 2,'unixTime': 3,
+		'msg_typ': 4, 'lat': 5,'long': 6,'model_id': 7,
+		'custm_msg': 8,'dtime': 9, 'bat': 10,'hidden': 11,
+		'alt': 12
+		}
 
 	def get_xml(self, URL):
 		"""
@@ -20,7 +27,23 @@ class ScrapeData():
 		"""
 		return XML.xpath('//message')
 
-	def make_kml_object(self):
+	def make_kml_object(self): # Temporary function to be deleted
 		kml = Kml()
 		kml.document.name = "Test"
-		kml.newpoint()
+		kml_points = map(self.create_one_point())
+
+	def create_one_point(self, kml, msg_data):
+		"""
+		returns a kml.newpoint object of the form
+		"""
+		point = kml.newpoint(name=self.get_date(msg_data[INDEX]))
+		return point
+
+
+	def get_time(self, msg_data):
+		""" currently missing 'position #' format """
+		# -13 and -8 indexes are consistent across all formats 
+		# I've seen. If this causes issues
+		# then regex should be used
+		time = msg_data[self.indx['dtime']][-13:-8]
+		return time
--- tests.py
+++ tests.py
@@ -8,6 +8,12 @@ class TestScraper(unittest.TestCase):
 		self.scraper = ScrapeData()
 		self.URL = ("https://api.findmespot.com/spot-main-web/consumer/" + 
 			"rest-api/2.0/public/feed/0i9SOoPmAWhrgM15Yl41McqjrEbERSGiD/message.xml")
+		self.indx = {
+		'id': 0, 'msngr_id': 1,'msngr_name': 2,'unixTime': 3,
+		'msg_typ': 4, 'lat': 5,'long': 6,'model_id': 7,
+		'custm_msg': 8,'dtime': 9, 'bat': 10,'hidden': 11,
+		'alt': 12
+		}
 
 	def test_gets_raw_xml_as_binary(self):
 		self.assertIsInstance(self.scraper.get_xml(self.URL), bytes)
@@ -35,6 +41,17 @@ class TestScraper(unittest.TestCase):
 	def test_intentional_fail(self):
 		with open('example_xml.xml','r') as f:
 			XML = fromstring(f.read())
-			for message in  self.scraper.get_coord_list(XML):
+			for message in  self.scraper.get_coord_list(XML)[2:]:
 				# print([x.text for x in message])
 				self.fail([x.text for x in message])
+
+	def test_gets_right_time(self):
+		# Need 2 samples for 'Position #' check
+		sample_row1 = ['913691903', '0-2440482', 'Lima', '1517941403', 
+		'UNLIMITED-TRACK', '-8.38843', '-74.63849', 'SPOT3', 'Y', 
+		'2018-02-06T18:23:23+0000', 'GOOD', '0', '644']
+		sample_row2 = ['913686008', '0-2440482', 'Lima', '1517940807', 
+		'UNLIMITED-TRACK', '-8.74288', '-74.43631', 'SPOT3', 'Y', 
+		'2018-02-06T18:13:27+0000', 'GOOD', '0', '2027']
+		self.assertEqual(self.scraper.get_time(sample_row1), '18:23')
+		self.assertEqual(self.scraper.get_time(sample_row2), '18:13')
